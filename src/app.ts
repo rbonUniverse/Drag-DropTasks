9d43d10806cc202e565c01c763afadc55b4c1c7d
rbonUniverse <robby.zigi@gmail.com>
2023-08-19 21:20:54 +0300

    added event listener to the form

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,34 @@
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
-    element: HTMLFormElement
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 constructor(){
     this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
     this.hostElement = document.getElementById('app') as HTMLDivElement;
-
+    
     const importNode = document.importNode(this.templateElement.content, true);
     this.element = importNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+    
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
 }
+
+private submitHandler (event: Event) {
+    event.preventDefault();
+}
+
+private configure () {
+this.element.addEventListener('submit', this.submitHandler.bind(this));
+}
+
 private attach(){
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
 }
